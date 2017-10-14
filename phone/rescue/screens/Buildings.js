82443ebff0160cad0041fb92060d23a42481c2fc
Andrew Gonzales <andrewmartingonzales@gmail.com>
2017-10-14 13:05:58 -0400

    Add mock data and picker to Buildings screen

--- phone/rescue/screens/Buildings.js
+++ phone/rescue/screens/Buildings.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Picker, StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
 	container: {
@@ -11,17 +10,93 @@ const styles = StyleSheet.create({
 	},
 });
 
+const BUILDINGS = [
+	{
+		address: '10 SOUTH STREET',
+		numberOfFloors: 5,
+		XCoord: 981037,
+		YCoord: 194506,
+		zip: 10004,
+		ownerName: '10 SSA LANDLORD, LLC',
+		healthArea: '7700',
+		policePrecinct: '1',
+		fireCompany: 'L015',
+		latitude: 40.70055018440018,
+		longitude: -74.0115876259819,
+	},
+	{
+		address: '15 SOUTH STREET',
+		numberOfFloors: 5,
+		XCoord: 981037,
+		YCoord: 194506,
+		zip: 10004,
+		ownerName: '15 SSA LANDLORD, LLC',
+		healthArea: '7700',
+		policePrecinct: '1',
+		fireCompany: 'L015',
+		latitude: 40.70055018440018,
+		longitude: -74.0115876259819,
+	},
+	{
+		address: '20 SOUTH STREET',
+		numberOfFloors: 5,
+		XCoord: 981037,
+		YCoord: 194506,
+		zip: 10004,
+		ownerName: '20 SSA LANDLORD, LLC',
+		healthArea: '7700',
+		policePrecinct: '1',
+		fireCompany: 'L015',
+		latitude: 40.70055018440018,
+		longitude: -74.0115876259819,
+	},
+	{
+		address: '25 SOUTH STREET',
+		numberOfFloors: 5,
+		XCoord: 981037,
+		YCoord: 194506,
+		zip: 10004,
+		ownerName: '25 SSA LANDLORD, LLC',
+		healthArea: '7700',
+		policePrecinct: '1',
+		fireCompany: 'L015',
+		latitude: 40.70055018440018,
+		longitude: -74.0115876259819,
+	},
+];
+
 export default class Emergency extends React.Component {
-	changeScreen = () => {
-		const { navigate } = this.props.navigation;
-		navigate('Map');
+	constructor() {
+		super();
+		this.state = { index: 0 };
+	}
+
+	// changeScreen = () => {
+	// 	const { navigate } = this.props.navigation;
+	// 	navigate('Map');
+	// }
+
+	updateIndex = (index) => {
+		this.setState({ index })
 	}
 
 	render() {
+		const { index } = this.state;
 		return (
-			<View style={styles.container}>
-				<Text>Building</Text>
-				<Text>Building</Text>
+			<View >
+				<Picker selectedValue={BUILDINGS[index]} onValueChange = {this.updateIndex}>
+					{
+						BUILDINGS.map((building, i) => {
+							return (
+								<Picker.Item
+									key={`${building.address}_${i}`}
+									label={building.address}
+									value={building.address}
+								/>
+							);
+						})
+					}
+				</Picker>
 			</View>
 		);
 	}
