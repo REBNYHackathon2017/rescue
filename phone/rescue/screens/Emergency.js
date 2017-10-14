4b801a4095f8d5217808ad4d2ae4f2c54429865e
Andrew Gonzales <andrewmartingonzales@gmail.com>
2017-10-14 11:06:30 -0400

    Add buttons to Emergency screen

--- phone/rescue/screens/Emergency.js
+++ phone/rescue/screens/Emergency.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class Emergency extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Does your emergency pose an immediate threat to someone's someone’s life, safety, health, or property?</Text>
-      </View>
-    );
-  }
-}
+import Button from '../components/CustomButton';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
+
+export default class Emergency extends React.Component {
+  render() {
+	return (
+	  <View style={styles.container}>
+		<Text>Does your emergency pose an immediate threat to someone’s life, safety, health, or property?</Text>
+		<Button color="gray" text="NO" />
+		<Button text="YES" />
+	  </View>
+	);
+  }
+}
