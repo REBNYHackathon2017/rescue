e3bea09e526391e5ca70aaa3995d0dfd6e4b2feb
Andrew Gonzales <andrewmartingonzales@gmail.com>
2017-10-14 11:05:54 -0400

    Create custom button component

---
+++ phone/rescue/components/CustomButton.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button } from 'react-native-elements';
+
+const styles = StyleSheet.create({
+	container: {
+	  flex: 1,
+	  backgroundColor: '#fff',
+	  alignItems: 'center',
+	  justifyContent: 'center',
+	},
+});
+
+
+export default class CustomButton extends React.Component {
+	render() {
+		const { color, icon, onPress, text } = this.props;
+		return (
+			<View>
+				<Button
+					backgroundColor={color === 'gray' ? 'gray' : 'red'}
+					buttonStyle={{
+						alignSelf: 'center',
+						margin: 10,
+					}}
+					large={true}
+					title={text}
+				/>
+			</View>
+		);
+	}
+}
